1885b9c9ce13bf8409016b268c0c3e925e8531a4
habib-SE <hab67603@gmail.com>
2023-10-20 00:11:34 +0500

    update navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { AiOutlineMenu, AiOutlineClose } from "../../node_modules/react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+} from "../../node_modules/react-icons/ai";
 
 const Navbar = () => {
   return (
     <div>
       <nav className="bg-white-600 p-4 imageWrapper  ">
-        <div className="container mx-auto flex items-center justify-between hidden md:flex">
+        <div className="container mx-auto  items-center justify-between hidden md:flex">
           {/* <!-- Logo on the left --> */}
           <div className="flex items-center">
             <a href="/" className="text-white text-xl font-semibold">
@@ -57,56 +60,57 @@ const Navbar = () => {
               FREE DOWNLOAD
             </a>
           </div>
-         
-        </div>
-        <div className="block md:hidden  mx-[650px] ">
-            <AiOutlineClose size={20}  color="white"/>
-            <AiOutlineMenu size={20}  color="white"/>
+          <div className=" group block md:hidden  justify-between ">
+            <AiOutlineClose size={20} color="white" />
+            <AiOutlineMenu size={20} color="white" />
           </div>
-        {/* Centered navigation links */}
+        </div>
 
-        <ul
-          className={"md:space-x-6 justify-between items-center sm:flex-col"}
-        >
-          <div className="flex items-center">
+        {/* Centered navigation links */}
+        <div className="md:hidden">
+          <div className="flex items-center mb-3">
             <a href="/" className="text-white text-xl font-semibold">
               Material Tailwind React
             </a>
           </div>
-          <li></li>
-          <li className="nav-item">
-            <a href="/" className="text-white hover:text-gray-300">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/" className="text-white hover:text-gray-300">
-              Service
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/" className="text-white hover:text-gray-300">
-              Profile
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/" className="text-white hover:text-gray-300">
-              Docs
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/" className="text-white hover:text-gray-300">
-              Contact
-            </a>
-          </li>
-          
-        </ul>
-         {/* Buttons on the right */}
-         <div className="w-full grid grid-cols text-right flex-col space-x-4">
-         <button className="py-2 text-white px-4 rounded-full font-bold hover:bg-blue-200">PRO VERSION</button>
-         <button className=" bg-blue-700 font-bold text-white py-2 px-4 rounded-full hover:bg-blue-600">FREE DOWNDLOAD</button>
+          <ul className=" justify-between space-x-4 items-center sm:flex-col w-[600px] h-[350px] border rounded-lg p-8 bg-white ">
+            <li></li>
+            <li className="nav-item p-2">
+              <a href="/" className="text-black ">
+                Home
+              </a>
+            </li>
+            <li className="nav-item p-2">
+              <a href="/" className="text-black ">
+                Profile
+              </a>
+            </li>
+            <li className="nav-item p-2">
+              <a href="/" className="text-black ">
+                Sign in
+              </a>
+            </li>
+            <li className="nav-item p-2">
+              <a href="/" className="text-black ">
+                Sign up
+              </a>
+            </li>
+            <li className="nav-item p-2">
+              <a href="/" className="text-black ">
+                Docs
+              </a>
+            </li>
+            {/* Buttons on the right */}
+            <div className="w-[100%] mt-4 grid">
+              <button className="py-2 text-blue-700 px-4 rounded-md font-bold hover:bg-blue-200">
+                PRO VERSION
+              </button>
+              <button className=" bg-blue-700 mt-2 font-bold text-white py-2 px-2 rounded-md hover:bg-blue-600">
+                FREE DOWNDLOAD
+              </button>
+            </div>
+          </ul>
         </div>
-       
       </nav>
     </div>
   );
