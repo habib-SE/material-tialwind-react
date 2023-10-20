5e09f8bdbc5d760d71a79cb9170a4844f1829068
habib-SE <hab67603@gmail.com>
2023-10-20 16:09:15 +0500

    add icons and set cnteral navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AiOutlineMenu,
   AiOutlineClose,
+  AiFillHome,
+  AiOutlineProfile,
+  AiOutlineMobile,
+  AiOutlineUserAdd,
+  AiOutlinePaperClip
 } from "../../node_modules/react-icons/ai";
 
+
+
+
 const Navbar = () => {
+
+  const [nav, setNav] = useState (false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div>
       <nav className="bg-white-600 p-4 imageWrapper  ">
@@ -20,7 +32,7 @@ const Navbar = () => {
           <ul className="flex space-x-8 ">
             <li className="nav-item">
               <a href="/" className="text-white hover:text-gray-300">
-                Home
+              <AiFillHome/> Home
               </a>
             </li>
             <li className="nav-item">
@@ -60,25 +72,24 @@ const Navbar = () => {
               FREE DOWNLOAD
             </a>
           </div>
-          <div className=" group block md:hidden  justify-between ">
-            <AiOutlineClose size={20} color="white" />
-            <AiOutlineMenu size={20} color="white" />
-          </div>
+         
         </div>
-
+        <div onClick={handleClick} className='md:hidden block  mx-[95%] mt-4 z-10'>
+           {!nav ?  <AiOutlineMenu size={20} color="white" /> :<AiOutlineClose size={20} color="white" />} 
+           
+          </div>
         {/* Centered navigation links */}
-        <div className="md:hidden">
-          <div className="flex items-center mb-3">
+        <div className="md:hidden relative top-[-2%]">
+          <div className="flex items-center mb-5">
             <a href="/" className="text-white text-xl font-semibold">
               Material Tailwind React
             </a>
           </div>
-          <ul className=" justify-between space-x-4 items-center sm:flex-col w-[600px] h-[350px] border rounded-lg p-8 bg-white ">
+          <ul className={!nav ? 'hidden': " justify-between space-x-4 items-center sm:flex-col w-[530px] h-[350px] border rounded-lg p-8 bg-white "}>
             <li></li>
             <li className="nav-item p-2">
               <a href="/" className="text-black ">
-                Home
-              </a>
+               <span><AiFillHome/></span>Home</a>
             </li>
             <li className="nav-item p-2">
               <a href="/" className="text-black ">
