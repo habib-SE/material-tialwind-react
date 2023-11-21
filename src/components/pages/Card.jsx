c7117d0216afedb2794ad186e53f5e5c437fa931
habib-SE <hab67603@gmail.com>
2023-11-21 22:21:21 +0500

    create a card component and copmlet

--- src/components/pages/Card.jsx
+++ src/components/pages/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { MdChatBubble } from '../../../node_modules/react-icons/md';
 
-const Card = () => {
+const Card = ({title, text}) => {
   return (
     <div>
-      
+      <div className="max-w-sm rounded overflow-hidden bg-white shadow-lg">
+        <div className=' ml-[40%] mt-[10%]'>
+        <MdChatBubble size={40} />
+        </div>
+        
+      <div className=" px-[4%] py-[4%]">
+        <div className="font-bold text-xl text-center mb-2">{title}</div>
+        <p className="text-gray-700 w-full text-base">{text}</p>
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,25 +1,45 @@
+// Home.js
+
 import React from "react";
 import Navbar from "../Navbar";
 import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const Home = () => {
   return (
     <div>
-      {/* Home text */}
-      <Link>
+      {/* Navbar */}
+      <Link to="/">
         <Navbar />
       </Link>
 
-      <div className=" text-white py-6 ">
+      {/* Upper Section */}
+      <div className="text-white py-6 relative">
         <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
           <h1 className="md:text-6xl sm:text-5xl text-3xl font-bold md:py-6">
             Your story starts with us.
           </h1>
           <p className="md:text-2xl text-xl font-bold text-gray-300">
-            his is a simple example of a Landing Page you can build using
+            This is a simple example of a Landing Page you can build using
             Material Tailwind. It features multiple components based on the
             Tailwind CSS and Material Design by Google.
           </p>
+        </div>
+      </div>
+
+      {/* Card Section */}
+      <div className="container mx-auto absolute top-[90%] inset-0 flex items-center justify-center z-10">
+        <div className="grid grid-cols-3 gap-10">
+          <div className="flex">
+            <Card title="Awarded Agency" text="Divide details about your product or agency work into parts. A paragraph describing a feature will be enough." />
+          </div>
+          <div className="flex">
+            <Card title="Free Revisions" text="
+          Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious." />
+          </div>
+          <div className="flex">
+            <Card title="Verified Company" text="Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!" />
+          </div>
         </div>
       </div>
     </div>
